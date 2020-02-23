e82d8d5b23a1f45d8fad12fe3a51ff1dae6a1051
Chris Carr <ccarr895@gmail.com>
2020-02-23 22:42:21 +0000

    added footer

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,4 +1,29 @@
-<footer class="footer">
+<footer class="footer mt-5 pt-2 rounded-top page-footer py-3" style="background-color: black">
+	<div class="container-fluid">
+		<div class="row pb-3">
+			<div class="col-4">
+				<address class="text-white">
+					<p class="text-bold text-capitalize"><b>Address:</b></p>
+					<p class="small">
+						MMU,<br>
+						Near oxford road,<br>
+						Just passed the bridge</p>
+				</address>
+			</div>
+			<div class="col-4">
+				<p class="text-bold text-white"><b>Email:</b> <a href="mailto://17105584@stu.mmu.ac.uk">17105584@stu.mmu.ac.uk</a></p>
+			</div>
+			<div class="col-4">
+				<div class="text-white">Some badges to show the site is certified: </div>
+				<div class="row mt-4 ml-1">
+					<!-- <img class='border-light border' alt="W3 Verification Badge" height="61" src="#" />
+					<img class="ml-4" alt="Google Lighthouse Logo" width="auto" height="61" src="/img/footer_badges/lighthouse-logo.svg" />
+					<img class="border-light border mt-3 ml-2" alt="Google Lighthouse Report" width="303" height="122" src="/img/footer_badges/lighthouse_report.PNG"/> -->
+				</div>
+			</div>
+		</div>
+		<div class="blockquote-footer text-center text-info border-top border-white pt-1">Created with ❤ by: <span class="text-white">Chris Carr</span></div>
+	</div>
 </footer>
 
 <script src="<?php echo base_url('libs/js/jquery.min.js') ?>" defer></script>
